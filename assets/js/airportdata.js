b0405d1a92f2d306279f75718be95acc9a162bbf
pmcgrenery <pmcgrenery@gmail.com>
2021-04-14 12:55:59 +0000

    Add airport function implemented

--- assets/js/airportdata.js
+++ assets/js/airportdata.js
@@ -1,4 +1,4 @@
-function fetchairportinfo(event) {
+function fetchairportinfo() {
     let icao = $('#airportInput').val();
 
     let settings = {
@@ -15,13 +15,37 @@ function fetchairportinfo(event) {
     $.ajax(settings).done(function (response) {
         console.log(response);
         if (response.error) {
+            //If an error is returned
             $("#not-airport").html("Airport does not exist, please try again");
 
-            //TO DO -> Clear the input
+            //TO DO -> 
+            // Form input validation...only accept 4 letters
+            //1. Present new modal "not an airport"
+            //2. CLear the enter airport modal
+            //3. When you press close, show the first modal
+
         };
         var latitude = response.latitude;
         var longitude = response.longitude;
-        var iata = response.iata;
+        var icaoCode = response.icao;
+        var iataCode = response.iata;
         var airportName = response.name;
+
+        addAirport(iataCode, icaoCode, airportName, response.error);
     });
 };
+
+// Function to append the new airport to the list
+function addAirport(iata, icao, apName, error) {
+    let apDetails = `<div class="airport-selector bg-green">
+                        <span>${icao} -</span><span> ${iata}</span>
+                        <p>${apName}</p>
+                    </div>`
+    // If the airport doesn't exist return out of the function
+    if (error) {
+        return;
+    }
+    $("#airports").append(apDetails);
+};
+
+//TODO -> Add event listener to accept the enter button as pressing the add airport button
