8aa5e4169082ca4c38a12fd2e1fb9ae2d12490b3
pmcgrenery <pmcgrenery@gmail.com>
2021-04-16 16:17:17 +0000

    Added function to get TAF and to set TAF and METAR on the DOM

--- assets/js/avwx.js
+++ assets/js/avwx.js
@@ -7,30 +7,66 @@ const tokenCode = "xxt-4d_jaDNW_tkNo2wX8q6cXUfWZ2asMCsmwYlr7Gw";
 
 //When document is ready getAirport Weather (AND RUNWAYS??????????????)
 $("document").ready(function () {
-    getAirportMetar();
-});
-
-function getAirportMetar() {
-    //Code modified from AVWX Documentation
-    //https://avwx.docs.apiary.io/#reference/0/station
-
     //Load the selected airport from local storage
     let airport = JSON.parse(localStorage.getItem('selectedAirport'));
     let icao = airport.icao;
 
+    getAirportMETAR(icao);
+    getAirportTAF(icao);
+});
+
+function getAirportMETAR(icaoCode) {
+    //Code modified from AVWX Documentation
+    //https://avwx.docs.apiary.io/#reference/0/metar/get-metar-report
+
+
+
+
     let request = new XMLHttpRequest();
-    request.open('GET', `https://avwx.rest/api/metar/${icao}?format=json`);
+    request.open('GET', `https://avwx.rest/api/metar/${icaoCode}?format=json`);
     request.setRequestHeader('Authorization', tokenCode);
     request.onreadystatechange = function () {
         if (this.readyState === 4) {
-            console.log('Status:', this.status);
-            console.log('Headers:', this.getAllResponseHeaders());
-            console.log('Body:', this.responseText);
-            let status = this.status;
-            let body = this.body;
+            // console.log('Status:', this.status);
+            // console.log('Headers:', this.getAllResponseHeaders());
+            // console.log('Body', this.responseText);
+            let status = JSON.parse(this.status);
+            let body = JSON.parse(this.responseText);
+            console.log(body);
+            //Call setMetar function
+            setMetar(status, body);
         }
     };
     request.send();
-    //Call function to set the METAR
-    // setWxRawData();
 };
+
+function setMetar(status, metar) {
+    $("#metar").html(metar.raw);
+}
+
+function getAirportTAF(icao) {
+    //Code modified from AVWX Documentation
+    //https://avwx.docs.apiary.io/#reference/0/taf/get-taf-report
+
+
+    let request = new XMLHttpRequest();
+    request.open('GET', `https://avwx.rest/api/taf/${icao}?format=json`);
+    request.setRequestHeader('Authorization', tokenCode);
+    request.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            // console.log('Status:', this.status);
+            // console.log('Headers:', this.getAllResponseHeaders());
+            // console.log('Body', this.responseText);
+            let status = JSON.parse(this.status);
+            let body = JSON.parse(this.responseText);
+            console.log(body);
+            //Call setMetar function
+            setTAF(status, body);
+        }
+    };
+    request.send();
+};
+
+function setTAF(status, taf) {
+    $("#taf").html(taf.raw);
+}
