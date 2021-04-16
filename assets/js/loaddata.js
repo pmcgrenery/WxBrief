d83498add8e7436ca44b07e8bc567401b30a6d42
pmcgrenery <pmcgrenery@gmail.com>
2021-04-16 15:51:05 +0000

    Renamed loaddata.js to selectairport.js

--- assets/js/loaddata.js
+++
@@ -1,7 +0,0 @@
-// Select Airport Click Event Listener
-$("#airports").on("click", ".airport-selector", function () {
-    console.log($(this).index());
-    // Create a global variable for the index of the selected airport
-    var apIndex = $(this).index();
-    window.location.href = "wxreport.html";
-});
---
+++ assets/js/selectairport.js
@@ -0,0 +1,12 @@
+// Select Airport Click Event Listener
+$("#airports").on("click", ".airport-selector", function () {
+    console.log($(this).index());
+    // Find the index of the selected airport
+    let airportIndex = $(this).index();
+    // Get the airport object for the selected airport
+    let selectedAirport = airports[airportIndex];
+    // Put the airport into local storage to be accessed in other js files
+    localStorage.setItem('selectedAirport', JSON.stringify(selectedAirport));
+    // Open the wxreport.html page
+    window.location.href = "wxreport.html"
+});
