7c3fd97ac68bb200ef57188bb2a605f6332680ff
pmcgrenery <pmcgrenery@gmail.com>
2021-04-16 13:53:29 +0000

    Created avwx.js

---
+++ assets/js/avwx.js
@@ -0,0 +1,9 @@
+const token = "xxt-4d_jaDNW_tkNo2wX8q6cXUfWZ2asMCsmwYlr7Gw";
+
+// API Info:
+// https://avwx.rest/#
+// Error code 429 if you hit call limit.
+// Limit is 4000 calls per day. Resets at 0000Z.
+
+
+console.log(apIndex);
