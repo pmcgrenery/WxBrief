0f728fbb85b04ff7fa7c00c68597769af97410b6
pmcgrenery <pmcgrenery@gmail.com>
2021-05-12 11:12:00 +0000

    Moved select airport function inot effects.js

--- assets/js/effects.js
+++ assets/js/effects.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
     menuSlide();
     setMargins();
     buttonClick();
+    selectAirport();
 });
 
 function scrollOpaqueHeader() {
@@ -98,6 +99,30 @@ function buttonClick() {
         // Follow the link
         setTimeout(function () {
             window.location.href = href;
+        }, 100)
+
+    });
+}
+
+function selectAirport() {
+    $("#airports").on("click", ".airport-text", function () {
+        let airports = JSON.parse(localStorage.getItem('airports'));
+        // Find the index of the selected airport
+        let airportIndex = $(this).parent().index();
+        // Get the airport object for the selected airport
+        let selectedAirport = airports[airportIndex];
+
+        //Make the button shrink then grow
+        $(this).parent().addClass("shrink");
+        $(this).delay(50).queue(function () {
+            $(this).parent().addClass("grow");
+        });
+
+        // Put the airport into local storage to be accessed in other js files
+        sessionStorage.setItem('selectedAirport', JSON.stringify(selectedAirport));
+        // Open the wxreport.html page
+        setTimeout(function () {
+            window.location.href = "wxreport.html"
         }, 100)
 
     });
