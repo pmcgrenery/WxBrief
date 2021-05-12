fa763b157d5df688a5cb2f3950d2d21bc8ee2e79
pmcgrenery <pmcgrenery@gmail.com>
2021-05-12 11:13:51 +0000

    Typo fixes and deleted selectairport.js

--- assets/js/avwx.js
+++ assets/js/avwx.js
@@ -1,4 +1,4 @@
-$("document").ready(function () {
+$(document).ready(function () {
     let airport = JSON.parse(sessionStorage.getItem('selectedAirport'));
     setHeader(airport);
     getAirportMETAR(airport.icao);
