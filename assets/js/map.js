420dafe66e813b9f3ead78b41aace91e8bbc846f
pmcgrenery <pmcgrenery@gmail.com>
2021-05-14 16:47:49 +0000

    Split rainviewer files to map.js and rainviewer.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,9 +1,52 @@
+$(document).ready(function () {
+    checkConfigLoaded();
+    initialiseEventListeners();
+});
+
+/**
+ * Checks that config.js file has loaded correctly
+ */
+function checkConfigLoaded() {
+    if (typeof LOADED !== 'undefined') {
+        checkHref();
+    } else if (typeof LOADED === 'undefined') {
+        $("#configNotLoadedModal").modal("show");
+    }
+}
+
+/**
+ * Checks the current page file name
+ */
+function checkHref() {
+    let file = location.pathname.split('/').pop();
+
+    if (file === "wxreport.html") {
+        mapWxReport();
+        setVersion(MAP_DEFAULT, file);
+
+    } else {
+        mapIndex();
+        checkIfLocationAllowed();
+        setVersion(MAP_DEFAULT);
+    }
+}
+
+function initialiseEventListeners() {
+    singleFingerDrag();
+    playPauseToggle();
+    radarOptionToggle();
+    legendToggler();
+    mapLayerToggler();
+    fullscreenToggler();
+};
+
 let map;
 
 /**
- * Creates Leaflet map and configures its control options
+ * Creates Leaflet map and tailors option to index page
  */
-function configureMap() {
+function mapIndex() {
+
     map = L.map('mapid', {
         center: [0, 0],
         zoom: 1.5,
@@ -21,4 +64,283 @@ function configureMap() {
         tap: !L.Browser.mobile,
         tap: !L.Browser.mobileWebkit,
     });
+
+};
+
+/**
+ * Creates Leaflet map and tailors option to Wxreport page
+ */
+function mapWxReport() {
+
+    if (map != undefined) {
+        map.remove();
+    }
+    airport = JSON.parse(sessionStorage.getItem('selectedAirport'));
+    lat = airport.lat;
+    long = airport.long;
+
+    map = L.map('mapid', {
+        center: [lat, long],
+        zoom: 7.5,
+        minZoom: 1,
+        maxBounds: [
+            [90, -180],
+            [-90, 180]
+        ],
+        maxBoundsViscosity: 1,
+        zoomSnap: 0,
+        zoomDelta: 0.5,
+        wheelPxPerZoomLevel: 80,
+        dragging: !L.Browser.mobile,
+        dragging: !L.Browser.mobileWebkit,
+        tap: !L.Browser.mobile,
+        tap: !L.Browser.mobileWebkit,
+    });
+};
+
+/**
+ * Clears all existing map layers then adds 
+ * baselayer, radar, radar mask and sigmets layers
+ * @param {string} base - The base layer of the Mapbox tiles
+ * @param {string} page - The current html page
+ * @example
+ *  setVersion(mapbox/light-v10, wxreport.html)
+ */
+function setVersion(base, page) {
+    map.eachLayer(function (layer) {
+        map.removeLayer(layer);
+    });
+    L.tileLayer.provider('MapBox', {
+        id: base,
+        accessToken: KEY.mapbox
+    }).addTo(map);
+    L.tileLayer(URL.rvMask).setOpacity(0.4).addTo(map);
+    initialize(apiData, optionKind);
+    getSigmet(base);
+    $(".base-wrapper").hide();
+    if (page === "wxreport.html") {
+        addMarker();
+    }
+};
+
+/**
+ * Displays the frame time in the radar controls
+ * @param {Object} frame - Object containing timestamp of current radar frame
+ */
+function setFrameTime(frame) {
+    let dateObj = new Date(frame.time * 1000);
+    let utcString = dateObj.toUTCString();
+    let frameTime = utcString.slice(17, 22);
+    document.getElementById("timestamp").innerHTML = `${frameTime} UTC`
+};
+
+/**
+ * Adds a marker over the airport
+ */
+function addMarker() {
+    //Marker over airport
+    airport = JSON.parse(sessionStorage.getItem('selectedAirport'));
+    lat = airport.lat;
+    long = airport.long;
+    L.marker([lat, long]).addTo(map)
+        .bindPopup(`${airport.icao}`);
+};
+
+/**
+ * Checks if the user has previously allowed/disallowed location data
+ */
+function checkIfLocationAllowed() {
+    let locationAllowed = localStorage.getItem('WxBriefLocationAllowed');
+    if (locationAllowed === null) {
+        $("#geolocate").modal("show");
+    } else if (locationAllowed === "true") {
+        getLocation();
+    } else if (locationAllowed === "false") {
+        showEurope();
+        $("#geolocate").modal("hide");
+    }
+};
+
+/**
+ * Stores a variable to record that location data 
+ * allowed and closes modal
+ */
+function allowLocation() {
+    locationAllowed = "true";
+    localStorage.setItem('WxBriefLocationAllowed', locationAllowed);
+    getLocation();
+    $("#geolocate").modal("hide");
+};
+
+/**
+ * Stores a variable to record that location data
+ * is not allowed
+ */
+function disallowLocation() {
+    locationAllowed = "false";
+    localStorage.setItem('WxBriefLocationAllowed', locationAllowed);
+    showEurope();
+};
+
+/**
+ * Flys down to display Europe in the radar window
+ */
+function showEurope() {
+    let corner1 = L.latLng(60, 13),
+        corner2 = L.latLng(35, -19),
+        bounds = L.latLngBounds(corner1, corner2);
+    map.flyToBounds(bounds);
+};
+
+/**
+ * Uses the geolocation API to get the users current location
+ */
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showPositionError);
+    } else {
+        console.log("Geolocation is not supported by this browser.");
+        showEurope();
+    }
+};
+
+/**
+ * After 0.5sec, flys down to the users current location over 3 seconds
+ * @param {Object} position - GeoLocationPosition
+ */
+function showPosition(position) {
+    setTimeout(function () {
+        map.flyTo([position.coords.latitude, position.coords.longitude], 7.5, {
+            animate: true,
+            duration: 3
+        })
+    }, 500);
+};
+
+/**
+ * Handles geolocation errors by defaulting to showEurope
+ * @param {Object} error 
+ */
+function showPositionError(error) {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            showEurope();
+            break;
+        case error.POSITION_UNAVAILABLE:
+            showEurope();
+            break;
+        case error.TIMEOUT:
+            showEurope();
+            break;
+        case error.UNKNOWN_ERROR:
+            showEurope();
+            break;
+    };
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Toggles the play/pause icon on the radar controls when clicked
+ */
+function playPauseToggle() {
+    $("#play-toggle").click(function () {
+        $(".playPause").toggle();
+    });
+};
+
+/**
+ * Animates the radar/satellite option toggler when clicked
+ */
+function radarOptionToggle() {
+    $("#sat").click(function () {
+        $("#radar").removeClass("selected-left");
+        $("#sat").addClass("selected-right");
+    });
+    $("#radar").click(function () {
+        $("#sat").removeClass("selected-right");
+        $("#radar").addClass("selected-left");
+    });
+};
+
+/**
+ * Toggles the legend display when clicked
+ */
+function legendToggler() {
+    $("#legend-control").click(function () {
+        $(".legend-wrapper").toggle();
+    });
+};
+
+/**
+ * Toggles the base layer options display when layers icon is clicked
+ */
+function mapLayerToggler() {
+    $(".base-control").click(function () {
+        $(".base-wrapper").toggle();
+    });
+};
+
+/**
+ * Toggles the radar window to fullscreen when the fullscreen 
+ * icon is clicked and enables single finger dragging when in fullscreen
+ */
+function fullscreenToggler() {
+    let fullscreen = false;
+    $("#fullscreen-control").click(function () {
+        $("#radar-map-container").toggleClass("fullscreen").toggleClass("radar-container");
+        $(".fs-icon").toggle();
+        map.invalidateSize();
+        if (fullscreen === false) {
+            map.dragging.enable();
+            fullscreen = true;
+        } else if (fullscreen === true) {
+            map.dragging.disable();
+            if ($(window).width() > 480) {
+                map.dragging.enable();
+            }
+            fullscreen = false;
+        }
+    });
+};
+
+/**
+ * Enables single finger dragging on screen sizes >480px
+ */
+function singleFingerDrag() {
+    if ($(window).width() > 480) {
+        map.dragging.enable();
+    };
 };
--- assets/js/rainviewer.js
+++ assets/js/rainviewer.js
@@ -1,312 +1,152 @@
-$('document').ready(function () {
-    configureMap();
-    setVersion(MAP_DEFAULT);
-    initialiseRvSettings();
-    getRainviewer();
-    addMarker();
-    initialiseEventListeners();
-});
+  /**
+   * RainViewer radar animation part
+   * @type {number[]}
+   */
+  let apiData = {};
+  let mapFrames = [];
+  let lastPastFramePosition = -1;
+  let radarLayers = [];
+  let optionKind = 'radar'; // can be 'radar' or 'satellite'
+  let animationPosition = 0;
+  let animationTimer = false;
 
-function initialiseEventListeners() {
-    singleFingerDrag();
-    playPauseToggle();
-    radarOptionToggle();
-    legendToggler();
-    mapLayerToggler();
-    fullscreenToggler();
-};
+  /**
+   * Load all the available maps frames from RainViewer API
+   */
+  let apiRequest = new XMLHttpRequest();
+  apiRequest.open("GET", URL.rv, true);
+  apiRequest.onload = function (e) {
+      // store the API response for re-use purposes in memory
+      apiData = JSON.parse(apiRequest.response);
+      initialize(apiData, optionKind);
+  };
+  apiRequest.send();
 
-let map, airport, lat, long;
+  /**
+   * Initialize internal data from the API response and options
+   */
+  function initialize(api, kind) {
+      // remove all already added tiled layers
+      for (let i in radarLayers) {
+          map.removeLayer(radarLayers[i]);
+      }
+      mapFrames = [];
+      radarLayers = [];
+      animationPosition = 0;
 
-/**
- * Creates Leaflet map and configures its control options
- */
-function configureMap() {
-    airport = JSON.parse(sessionStorage.getItem('selectedAirport'));
-    lat = airport.lat;
-    long = airport.long;
+      if (!api) {
+          return;
+      }
+      if (kind == 'satellite' && api.satellite && api.satellite.infrared) {
+          mapFrames = api.satellite.infrared;
 
-    map = L.map('mapid', {
-        center: [lat, long],
-        zoom: 7.5,
-        minZoom: 1,
-        maxBounds: [
-            [90, -180],
-            [-90, 180]
-        ],
-        maxBoundsViscosity: 1,
-        zoomSnap: 0,
-        zoomDelta: 0.5,
-        wheelPxPerZoomLevel: 80,
-        dragging: !L.Browser.mobile,
-        dragging: !L.Browser.mobileWebkit,
-        tap: !L.Browser.mobile,
-        tap: !L.Browser.mobileWebkit,
-    });
-};
+          lastPastFramePosition = api.satellite.infrared.length - 1;
+          showFrame(lastPastFramePosition);
+      } else if (api.radar && api.radar.past) {
+          mapFrames = api.radar.past;
+          lastPastFramePosition = api.radar.past.length - 1;
+          showFrame(lastPastFramePosition);
+      }
+  }
 
-/**
- * Clears all existing map layers then adds 
- * baselayer, radar, radar mask and sigmets layers
- * @param {string} base - The base layer of the Mapbox tiles
- * @example
- *  setVersion(mapbox/light-v10)
- */
-function setVersion(base) {
-    map.eachLayer(function (layer) {
-        map.removeLayer(layer);
-    });
-    L.tileLayer.provider('MapBox', {
-        id: base,
-        accessToken: KEY.mapbox
-    }).addTo(map);
-    L.tileLayer(URL.rvMask).setOpacity(0.4).addTo(map);
-    initialize(apiData, optionKind);
-    getSigmet(base);
-    addMarker();
-    $(".base-wrapper").hide();
-};
+  /**
+   * Animation functions
+   * @param path - Path to the XYZ tile
+   */
+  function addLayer(frame) {
+      if (!radarLayers[frame.path]) {
+          let colorScheme = optionKind == 'satellite' ? 0 : 7;
+          let smooth = optionKind == 'satellite' ? 0 : 1;
+          let snow = optionKind == 'satellite' ? 0 : 1;
 
-/**
- * Displays the frame time in the radar controls
- * @param {Object} frame - Object containing timestamp of current radar frame
- */
-function setFrameTime(frame) {
-    let dateObj = new Date(frame.time * 1000);
-    let utcString = dateObj.toUTCString();
-    let frameTime = utcString.slice(17, 22);
-    document.getElementById("timestamp").innerHTML = `${frameTime} UTC`
-};
+          radarLayers[frame.path] = new L.TileLayer(apiData.host + frame.path + '/' + 512 + '/{z}/{x}/{y}/' + colorScheme + '/' + smooth + '_' + snow + '.png', {
+              tileSize: 256,
+              opacity: 0.001,
+              zIndex: frame.time
+          });
+      }
+      if (!map.hasLayer(radarLayers[frame.path])) {
+          map.addLayer(radarLayers[frame.path]);
+      }
+  }
 
-/**
- * Toggles the play/pause icon on the radar controls when clicked
- */
-function playPauseToggle() {
-    $("#play-toggle").click(function () {
-        $(".playPause").toggle();
-    });
-};
+  /**
+   * Display particular frame of animation for the @position
+   * If preloadOnly parameter is set to true, the frame layer only adds for the tiles preloading purpose
+   * @param position
+   * @param preloadOnly
+   */
+  function changeRadarPosition(position, preloadOnly) {
+      while (position >= mapFrames.length) {
+          position -= mapFrames.length;
+      }
+      while (position < 0) {
+          position += mapFrames.length;
+      }
 
-/**
- * Animates the radar/satellite option toggler when clicked
- */
-function radarOptionToggle() {
-    $("#sat").click(function () {
-        $("#radar").removeClass("selected-left");
-        $("#sat").addClass("selected-right");
-    });
-    $("#radar").click(function () {
-        $("#sat").removeClass("selected-right");
-        $("#radar").addClass("selected-left");
-    });
-};
+      let currentFrame = mapFrames[animationPosition];
+      let nextFrame = mapFrames[position];
 
-/**
- * Toggles the legend display when clicked
- */
-function legendToggler() {
-    $("#legend-control").click(function () {
-        $(".legend-wrapper").toggle();
-    });
-};
+      addLayer(nextFrame);
 
-/**
- * Toggles the base layer options display when layers icon is clicked
- */
-function mapLayerToggler() {
-    $(".base-control").click(function () {
-        $(".base-wrapper").toggle();
-    });
-};
+      if (preloadOnly) {
+          return;
+      }
 
-/**
- * Toggles the radar window to fullscreen when the fullscreen 
- * icon is clicked and enables single finger dragging when in fullscreen
- */
-function fullscreenToggler() {
-    let fullscreen = false;
-    $("#fullscreen-control").click(function () {
-        $("#radar-map-container").toggleClass("fullscreen").toggleClass("radar-container");
-        $(".fs-icon").toggle();
-        map.invalidateSize();
-        if (fullscreen === false) {
-            map.dragging.enable();
-            fullscreen = true;
-        } else if (fullscreen === true) {
-            map.dragging.disable();
-            if ($(window).width() > 480) {
-                map.dragging.enable();
-            }
-            fullscreen = false;
-        }
-    });
-};
+      animationPosition = position;
 
-/**
- * Enables single finger dragging on screen sizes >480px
- */
-function singleFingerDrag() {
-    if ($(window).width() > 480) {
-        map.dragging.enable();
-    };
-};
+      if (radarLayers[currentFrame.path]) {
+          radarLayers[currentFrame.path].setOpacity(0);
 
-function addMarker() {
-    //Marker over airport
-    L.marker([lat, long]).addTo(map)
-        .bindPopup(`${airport.icao}`);
-};
+      }
+      radarLayers[nextFrame.path].setOpacity(0.7);
 
-/**
- * Code below this point is from the RAINVIEWER API documentation,
- * the code has been modified slightly to suit the purposes of this site.
- */
+      setFrameTime(nextFrame)
+  }
 
-let apiData, mapFrames, radarLayers, lastPastFramePosition, optionKind, animationPosition, animationTimer;
+  /**
+   * Check avialability and show particular frame position from the timestamps list
+   */
+  function showFrame(nextPosition) {
+      let preloadingDirection = nextPosition - animationPosition > 0 ? 1 : -1;
 
-function initialiseRvSettings() {
-    apiData = {};
-    mapFrames = [];
-    radarLayers = [];
-    lastPastFramePosition = -1;
-    optionKind = 'radar';
-    animationPosition = 0;
-    animationTimer = false;
-}
+      changeRadarPosition(nextPosition);
 
-function getRainviewer() {
-    let apiRequest = new XMLHttpRequest();
-    apiRequest.open("GET", URL.rv, true);
-    apiRequest.onload = function () {
-        apiData = JSON.parse(apiRequest.response);
-        initialize(apiData, optionKind);
-    };
-    apiRequest.send();
-}
+      // preload next next frame (typically, +1 frame)
+      // if don't do that, the animation will be blinking at the first loop
+      changeRadarPosition(nextPosition + preloadingDirection, true);
+  }
 
-/**
- * Initialize internal data from the API response and options
- */
-function initialize(api, kind) {
-    for (let i in radarLayers) {
-        map.removeLayer(radarLayers[i]);
-    }
-    mapFrames = [];
-    radarLayers = [];
-    animationPosition = 0;
+  /**
+   * Stop the animation
+   * Check if the animation timeout is set and clear it.
+   */
+  function stop() {
+      if (animationTimer) {
+          clearTimeout(animationTimer);
+          animationTimer = false;
+          return true;
+      }
+      return false;
+  }
 
-    if (!api) {
-        return;
-    }
-    if (kind == 'satellite' && api.satellite && api.satellite.infrared) {
-        mapFrames = api.satellite.infrared;
-        lastPastFramePosition = api.satellite.infrared.length - 1;
-        showFrame(lastPastFramePosition);
-    } else if (api.radar && api.radar.past) {
-        mapFrames = api.radar.past;
-        lastPastFramePosition = api.radar.past.length - 1;
-        showFrame(lastPastFramePosition);
-    }
-}
+  function play() {
+      showFrame(animationPosition + 1);
 
-/**
- * Animation functions
- * @param path - Path to the XYZ tile
- */
-function addLayer(frame) {
-    if (!radarLayers[frame.path]) {
-        let colorScheme = optionKind == 'satellite' ? 0 : 7;
-        let smooth = optionKind == 'satellite' ? 0 : 1;
-        let snow = optionKind == 'satellite' ? 0 : 1;
+      // Main animation driver. Run this function every 200 ms
+      animationTimer = setTimeout(play, 200);
+  }
 
-        radarLayers[frame.path] = new L.TileLayer(apiData.host + frame.path + '/' + 512 +
-            '/{z}/{x}/{y}/' + colorScheme + '/' + smooth + '_' + snow + '.png', {
-                tileSize: 256,
-                opacity: 0.001,
-                zIndex: frame.time
-            });
-    }
-    if (!map.hasLayer(radarLayers[frame.path])) {
-        map.addLayer(radarLayers[frame.path]);
-    }
-}
+  function playStop() {
+      if (!stop()) {
+          play();
+      }
+  }
 
-/**
- * Display particular frame of animation for the @position
- * If preloadOnly parameter is set to true, the frame layer only adds for the tiles preloading purpose
- * @param position
- * @param preloadOnly
- */
-function changeRadarPosition(position, preloadOnly) {
-    while (position >= mapFrames.length) {
-        position -= mapFrames.length;
-    }
-    while (position < 0) {
-        position += mapFrames.length;
-    }
-
-    let currentFrame = mapFrames[animationPosition];
-    let nextFrame = mapFrames[position];
-
-    addLayer(nextFrame);
-
-    if (preloadOnly) {
-        return;
-    }
-
-    animationPosition = position;
-
-    if (radarLayers[currentFrame.path]) {
-        radarLayers[currentFrame.path].setOpacity(0);
-    }
-    radarLayers[nextFrame.path].setOpacity(0.6);
-
-    setFrameTime(nextFrame)
-}
-
-/**
- * Check avialability and show particular frame position from the timestamps list
- */
-function showFrame(nextPosition) {
-    let preloadingDirection = nextPosition - animationPosition > 0 ? 1 : -1;
-
-    changeRadarPosition(nextPosition);
-
-    // preload next next frame (typically, +1 frame)
-    // if don't do that, the animation will be blinking at the first loop
-    changeRadarPosition(nextPosition + preloadingDirection, true);
-}
-
-/**
- * Stop the animation
- * Check if the animation timeout is set and clear it.
- */
-function stop() {
-    if (animationTimer) {
-        clearTimeout(animationTimer);
-        animationTimer = false;
-        return true;
-    }
-    return false;
-}
-
-function play() {
-    showFrame(animationPosition + 1);
-
-    // Main animation driver. Run this function every 200ms ms
-    animationTimer = setTimeout(play, 200);
-}
-
-function playStop() {
-    if (!stop()) {
-        play();
-    }
-}
-
-/**
- * Radar/Satellite Option
- */
-function setKind(kind) {
-    optionKind = kind;
-    initialize(apiData, optionKind);
-};
+  /**
+   * Change map options
+   */
+  function setKind(kind) {
+      optionKind = kind;
+      initialize(apiData, optionKind);
+  }
